ab384757182666986b9412f0a231ae2c48899a3d
bsze2vaslas <bsze2vaslas@SZT1-19-HP>
2023-03-20 17:44:54 +0100

    oop programozas

--- bevezeto-projekt/bevezeto-projekt/Program.cs
+++ bevezeto-projekt/bevezeto-projekt/Program.cs
@@ -39,3 +39,14 @@ public class Dog
 
 }
 
+// O1.05 OOP programozás
+// Az OOP programozás során a programokat objektumokból építjük fel.
+//  - Minden objektumnak megvan a jól meghatározott feladata.
+//  - Az objektumok képesek egymással kommunikálni.
+//  - OOP programozás során a programozást közelítjük az emberi gondolkodáshoz.
+
+// O1.06 Példányosítás
+// Cat osztály példányosítása (create instance of class).
+//  - A myCat objektum a Cat osztály egy példánya.
+//  - A lokális változókat írásakor a caneCaseing jelölést használunk.
+Cat myCat = new Cat();
