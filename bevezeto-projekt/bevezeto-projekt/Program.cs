0ae2c2c47423ed880e71e13beeb376dd5dc2d486
bsze2vaslas <bsze2vaslas@SZT1-19-HP>
2023-03-20 17:21:43 +0100

    Cat osztaly

--- bevezeto-projekt/bevezeto-projekt/Program.cs
+++ bevezeto-projekt/bevezeto-projekt/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/// <summary>
+/// O1.01 Cat(macska) osztály
+/// Az osztály egy kiterjesztett adat típus. Az osztály egy program kód sablon, amelyben 
+/// állapotokat (adatokat) és viselkedési mintákat, műveleteket (metódusokat) lehet definiálni.
+/// Az osztályok neveit nagybetűvel (PascalCase) írjuk.
+/// Használjunk főneveket az osztály nevének megadásakor.
+/// </summary>
+public class Cat
+{
+
+}
+
